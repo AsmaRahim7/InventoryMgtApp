766b826463fb1432f47997c015ec842c3349b8f6
AsmaRahim7 <77214826+AsmaRahim7@users.noreply.github.com>
2022-08-04 21:43:14 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@ app.controller('ctrl-home', function ($scope) {
         let resp = await octokit.request('PUT /repos/AsmaRahim7/InventoryMgtApp/contents/categories.json?ref=main', {
             owner: 'AsmaRahim7',
             repo: 'InventoryMgtApp',
-            //path: 'categories.json',
+            path: 'https://github.com/AsmaRahim7/InventoryMgtApp/blob/main/categories.json',
             message: 'Saved categories',
             //committer: {
               //  name: 'Asma Rahim',

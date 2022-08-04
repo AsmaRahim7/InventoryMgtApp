16e340fcc41d64761017217c4709342720d536a3
AsmaRahim7 <77214826+AsmaRahim7@users.noreply.github.com>
2022-08-04 17:26:40 +0100

    Implement Functionality about json file storage

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
 var app = angular.module('inventory-app', []);
 var map;
 
@@ -20,6 +22,72 @@ app.controller('ctrl-home', function ($scope) {
     $scope.filteredItems = [];
     $scope.Categories = []
     $scope.CatName = '';
+
+    $scope.saveToServer = async function () {
+        // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+        const octokit = new Octokit({ auth: `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` });
+
+        // Save categories
+        // Get Json file
+        let file = await octokit.request('GET /repos/AsmaRahim7/InventoryMgtApp/contents/categories.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+        })
+
+        let resp = await octokit.request('PUT /repos/AsmaRahim7/InventoryMgtApp/contents/categories.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+            message: 'Saved categories',
+            content: btoa(JSON.stringify($scope.Categories)),
+            sha: file.data.sha
+        })
+
+        //Save Items
+        let file_items = await octokit.request('GET /repos/AsmaRahim7/InventoryMgtApp/contents/Items.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+        })
+
+        resp = await octokit.request('PUT /repos/AsmaRahim7/InventoryMgtApp/contents/Items.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+            message: 'Saved Items',
+            content: btoa(JSON.stringify($scope.items)),
+            sha: file_items.data.sha
+        })
+
+        alert('Your data has been saved to server.');
+    }
+
+    $scope.getFromServer = async function () {
+        const octokit = new Octokit({ auth: `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` });
+
+        let file_category = await octokit.request('GET /repos/AsmaRahim7/InventoryMgtApp/contents/categories.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+        })
+
+        let file_items = await octokit.request('GET /repos/AsmaRahim7/InventoryMgtApp/contents/Items.json', {
+            owner: 'AsmaRahim7',
+            repo: 'InventoryMgtApp',
+            path: 'PATH',
+        })
+
+        $scope.items = JSON.parse(atob(file_items.data.content));
+        $scope.Categories = JSON.parse(atob(file_category.data.content));
+
+        localStorage.setItem('categories', JSON.stringify($scope.Categories));
+        localStorage.setItem('items', JSON.stringify($scope.items));
+
+        //location.href = 'index.html';
+    }
+
+    //$scope.getFromServer();
 
     if (localStorage.getItem('items') !== null && localStorage.getItem('items') !== undefined) {
         $scope.items = JSON.parse(localStorage.getItem('items'));
